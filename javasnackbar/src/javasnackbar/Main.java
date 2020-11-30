060d1b5e3ffa1004f9974f8c8b98ed9c8a592398
Darren Tebo <darrentebo83@outlook.com>
2020-11-30 15:40:01 -0500

    created all required object instances

--- javasnackbar/src/javasnackbar/Main.java
+++ javasnackbar/src/javasnackbar/Main.java
@@ -1,5 +1,25 @@
 package javasnackbar;
 
 public class Main {
-    
+    public void processData(){
+        Customer c1 = new Customer("Jane", 45.25);
+        Customer c2 = new Customer("Bob", 33.14);
+
+        VendingMachine v1 = new VendingMachine("Food");
+        VendingMachine v2 = new VendingMachine("Drink");
+        VendingMachine v3 = new VendingMachine("Office");
+
+        Snack s1 = new Snack("Chips", 36, 1.75, v1.getId());
+        Snack s2 = new Snack("Chocolate Bar", 36, 1.00, v1.getId());
+        Snack s3 = new Snack("Pretzel", 30, 2.00, v1.getId());
+
+        Snack s4 = new Snack("Soda", 24, 2.50, v2.getId());
+        Snack s5 = new Snack("Water", 20, 2.75, v2.getId());
+
+        
+    }
+
+    public static void main(String[] args){
+        processData();
+    }
 }
